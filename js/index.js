be242630a1bdcc444e7ccfd3b45e007d6a1d1ae1
shonuff1221 <shonuff4521@gmail.com>
2021-03-30 05:03:26 -0400

    Media Page

--- js/index.js
+++ js/index.js
@@ -273,9 +273,9 @@ async function getUserAvailable() {
   
   return new Promise(async (resolve, reject) => {
     let data=await mainContract.methods.getUserAvailable(user.address).call();
-    
+    dataTrunc = data / 1e18;
     //console.log("available",data);
-    $("#getUserAvailable").text(web3.utils.fromWei(data,"ether")+" "+"BNB");
+    $("#getUserAvailable").text(abbreviate_number(dataTrunc, 4)+" "+"BNB");
   })
 }
 async function getUserReferralBonus() {
@@ -400,9 +400,11 @@ function clearSelection() {
     }
 }
 async function contractBalances(){
-	let contractBalance = (await mainContract.methods.getContractBalance().call() / 1e18)
+	let contractBalanceFull = (await mainContract.methods.getContractBalance().call() / 1e18)
+	let contractBalance = abbreviate_number(contractBalanceFull, 4)
 	$('#balanceContract').text(contractBalance)
-	let totalStaked = (await mainContract.methods.totalStaked().call() / 1e18)
+	let totalStakedFull = (await mainContract.methods.totalStaked().call() / 1e18)
+	let totalStaked = abbreviate_number(totalStakedFull, 4)
 	$('#totalStaked').text(totalStaked)
 }
 async function planPercents() {
@@ -470,6 +472,24 @@ function validateErcAddress(address) {
         return true;
 
     return false;
+}
+
+function abbreviate_number(_num, fixed) {
+    let num = parseFloat(_num)
+    if (num === null) {
+        return null;
+    } // terminate early
+    if (num === 0) {
+        return '0';
+    } // terminate early
+    fixed = (!fixed || fixed < 0) ? 0 : fixed; // number of decimal places to show
+    var b = (num).toPrecision(2).split("e"), // get power
+        k = b.length === 1 ? 0 : Math.floor(Math.min(b[1].slice(1), 14) / 3), // floor at decimals, ceiling at trillions
+        c = k <= 10 ? num.toFixed(0 + fixed) : (num / Math.pow(10, k * 3)).toFixed(1 + fixed), // divide by power
+        d = c < 0 ? c : Math.abs(c); // enforce -0 is 0
+        e = d// + ['', 'K', 'M', 'B', 'T'][k]; // append power
+
+    return e;
 }
 
 //TIMER
